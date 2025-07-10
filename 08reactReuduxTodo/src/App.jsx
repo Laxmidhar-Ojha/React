4dba4247ae870cadab87ba21c82ad1183d866db3
Bantyb792 <bantyb792@gmail.com>
2025-07-10 23:32:56 +0530

    learning react-redux-toolkit

---
+++ 08reactReuduxTodo/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
+
+const App=()=> {
+  return (
+    <>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
