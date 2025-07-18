97a88719b74b1321da37bb8bb28b032d632f0b58
Bantyb792 <bantyb792@gmail.com>
2025-07-18 17:56:31 +0530

    appwrite configuration and auth implementation

---
+++ 09MegaBlogProject/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+console.log(import.meta.env.VITE_APPWRITE_URL);
+function App() {
+
+  return (
+    <>
+     <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 09MegaBlogProject/src/appwrite/auth.js
@@ -0,0 +1,51 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+        client=new Client();
+        account;
+
+        constructor(){
+            this.client.setProject(conf.appwriteProjectId);
+            this.account=new Account(this.client);
+        }
+
+        createAccount=async ({email,password,name})=>{
+            try{
+                const userAcount=this.account.create(ID.unique(),email,password,name);
+                if(userAcount) return this.login(email,password)
+                else return userAcount
+            }catch(error){
+                throw error;
+            }
+        }
+
+        login = async({email,password})=>{
+            try {
+                return this.account.createEmailPasswordSession(email,password)
+            } catch (error) {
+                throw error
+            }
+        }
+
+        getCurrentUser= async()=>{
+            try {
+                return await this.account.get()
+            } catch (error) {
+                throw error
+            }
+            return null
+        }
+
+        logout= async()=>{
+            try {
+                await this.account.deleteSessions();
+            } catch (error) {
+                throw error
+            }
+        }
+}
+
+const authService=new AuthService();
+
+export default authService
